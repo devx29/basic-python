7383ef656be164d3ef8f2da8abf779860d9d9402
harsh2dev <harsh@friendemic.com>
2024-02-14 14:03:04 -0500

    first commit with basics covered

---
+++ first.py
@@ -0,0 +1,20 @@
+greeting = "Hello World!"
+print(greeting)
+
+print('Variables can consist of letters, numbers and underscore "_" but it can only start with a letter or underscore "_"')
+letters_1 = "A-Z,a-z"
+_numbers1 = "0-9"
+
+X = 4
+Y = 7
+Z = 6.9
+
+t = True
+f = False
+
+print("X is 4", "|", "Y is 7 " + "| Z is 6.9")
+print("X + Y =", X+Y, "\nX - Y =", X-Y, "\nX * Y =", X*Y, "\nY / X =", Y/X, "\nY squared equals", Y**2, "\nZ divided by X is", Z/X)
+print("*******************")
+print('t is TRUE and f is FALSE')
+print('T and F is', t and f, "\nT or F is", t or f)
+
