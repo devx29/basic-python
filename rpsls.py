54ca381bd7ee89afca7dec1d8bbc0b80d7805abf
harsh2dev <harsh@friendemic.com>
2024-02-15 16:03:34 -0500

    modifying-game-code

--- rpsls.py
+++ rpsls.py
@@ -10,44 +10,90 @@ class GAME(Enum):
     LIZARD = 4
     SPOCK = 5
 
-print("Choose your move! [0 for RULES!]")
-player_input = input("1. ROCK 🪨\n2. PAPER 📄\n3. SCISSORS 🗡️\n4. LIZARD 🦎\n5. SPOCK 🖖\n")
+PLAYER = 0
+SHELDON = 0
 
-if (int(player_input) == 0):
-    rules = '''
-    Scissors cuts paper.
-    Paper covers rock.
-    Rock crushes lizard.
-    Lizard poisons Spock.
-    Spock smashes scissors.
-    Scissors decapitates lizard.
-    Lizard eats paper.
-    Paper disproves Spock.
-    Spock vaporizes rock.
-    Rock crushes scissors.
-    '''
-    sys.exit(rules)
-elif ((int(player_input) <= 5) and (int(player_input) >= 1)):
-    player_choice = int(player_input)
-else:
-    sys.exit("🙏 Please enter an integer value between 0 and 5 🙏")
+replay = True
 
-computer_choice = random.randint(1,5)
+while replay:
 
-print("You chose: ", GAME(player_choice).name)
-print("Sheldon chose: ", GAME(computer_choice).name)
+    print("\nChoose your move! [0 for RULES!]")
+    player_input = input("\n1. ROCK 🪨\n2. PAPER 📄\n3. SCISSORS 🗡️\n4. LIZARD 🦎\n5. SPOCK 🖖\n\n")
 
-if player_choice == computer_choice:
-    sys.exit("It's a tie!")
+    if (int(player_input) == 0):
+        rules = '''
+        Scissors cuts paper.
+        Paper covers rock.
+        Rock crushes lizard.
+        Lizard poisons Spock.
+        Spock smashes scissors.
+        Scissors decapitates lizard.
+        Lizard eats paper.
+        Paper disproves Spock.
+        Spock vaporizes rock.
+        Rock crushes scissors.
+        '''
+        print(rules)
+        continue
+    elif ((int(player_input) <= 5) and (int(player_input) >= 1)):
+        player_choice = int(player_input)
+    else:
+        sys.exit("🙏 Please enter an integer value between 0 and 5 🙏")
 
-match player_choice:
-    case 1:
-        print("You win!") if computer_choice in (3,4) else print("You lose!")
-    case 2:
-        print("You win!") if computer_choice in (1,5) else print("You lose!")
-    case 3:
-        print("You win!") if computer_choice in (2,4) else print("You lose!")
-    case 4:
-        print("You win!") if computer_choice in (2,5) else print("You lose!")
-    case 5:
-        print("You win!") if computer_choice in (1,3) else print("You lose!")
+    computer_choice = random.randint(1,5)
+
+    print("\nYou chose: ", GAME(player_choice).name)
+    print("\nSheldon chose: ", GAME(computer_choice).name)
+
+    if player_choice == computer_choice:
+        print("\n🤝 It's a tie! 🤝")
+        continue
+
+    match player_choice:
+        case 1:
+            if computer_choice in (3,4):
+                print("\n 🏆 You win! 🏆")
+                PLAYER += 1
+            else:
+                print("\n😭 You lose! 😭")
+                SHELDON += 1
+        case 2:
+            if computer_choice in (1,5):
+                print("\n 🏆 You win! 🏆")
+                PLAYER += 1
+            else:
+                print("\n😭 You lose! 😭")
+                SHELDON += 1
+        case 3:
+            if computer_choice in (2,4):
+                print("\n 🏆 You win! 🏆")
+                PLAYER += 1
+            else:
+                print("\n😭 You lose! 😭")
+                SHELDON += 1
+        case 4:
+            if computer_choice in (2,5):
+                print("\n 🏆 You win! 🏆")
+                PLAYER += 1
+            else:
+                print("\n😭 You lose! 😭")
+                SHELDON += 1
+        case 5:
+            if computer_choice in (1,3):
+                print("\n 🏆 You win! 🏆")
+                PLAYER += 1
+            else:
+                print("\n😭 You lose! 😭")
+                SHELDON += 1
+
+    print('\nCurrent Score: \nYOU     =>', PLAYER, '\nSHELDON =>', SHELDON)
+    replay = input("\nPlay again? \nY for Yes | Enter any thing else to Quit\n\n")
+
+    if replay.lower() == "y":
+        continue
+    else:
+        print("\n❗️F I N A L  S C O R E S❗️".center(30, " "))
+        print("\nP L A Y E R".ljust(26, " ") + str(PLAYER).rjust(2))
+        print("\nS H E L D O N".ljust(26, " ") + str(SHELDON).rjust(2))
+        print("\n🫶  Thank you for playing! 🫶\n")
+        replay = False
